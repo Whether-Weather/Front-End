310d6ccd7a26e796b647dd1e743f2386bbf463a2
Josh Kelleran <josh.kelleran@gmail.com>
2023-03-16 16:22:13 -0700

    working map version

--- src/pages/LiveMap.js
+++ src/pages/LiveMap.js
@@ -9,14 +9,19 @@ const COUNTRIES =
 const AIR_PORTS =
   'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson'
 
+const DATA_URL = '/data/oct11_test.json';
+
+const USMAP = '/data/us_states.json'
+
 // Viewport settings
 const INITIAL_VIEW_STATE = {
-    latitude: 51.47,
-    longitude: 0.45,
-    zoom: 4,
-    bearing: 0,
-    pitch: 30,
-  }
+  longitude: -120.7401,
+  latitude: 47.7511,
+  zoom: 6,
+  maxZoom: 16,
+  pitch: 0,
+  bearing: 0
+};
 
 // Data to be used by the LineLayer
 // const data = [
@@ -25,57 +30,47 @@ const INITIAL_VIEW_STATE = {
 
 // DeckGL react component
 function Livemap() {
-  // Get the height of the navbar
-  const navbar = document.querySelector('.navbar-content-area');
-  const navbarHeight = navbar ? navbar.offsetHeight : 0;
-  console.log(navbarHeight);
+   
+  function colorToRGBArray(speed) {
+    if (speed[0] < 30) {
+        return [255,0,0];
+    }
+    else if (speed[0] >= 30 && speed[0] < 50) {
+        return [255,128,0];
+    }
+    else if (speed[0] >= 50 && speed[0] < 70) {
+        return [255,255,0];
+    }
+    else if (speed[0] >= 70 && speed[0] < 80) {
+        return [128,255,0];
+    }
+    else if (speed[0] >= 80) {
+        return [0,255,0];
+    }
+}
 
-  // Calculate the height of the DeckGL component
-  const windowHeight = window.innerHeight;
-  const deckglHeight = windowHeight - navbarHeight;
-
- 
   const layers = [
     new GeoJsonLayer({
-              id: 'base-map',
-              data: COUNTRIES,
-              // Styles
-              stroked: true,
-              filled: true,
-              lineWidthMinPixels: 2,
-              opacity: 0.4,
-              getLineColor: [60, 60, 60],
-              getFillColor: [200, 200, 200],
-            }),
-            new GeoJsonLayer({
-              id: 'airports',
-              data: AIR_PORTS,
-              // Styles
-              filled: true,
-              pointRadiusMinPixels: 2,
-              pointRadiusScale: 2000,
-              getFillColor: [200, 0, 80, 180],
-              // Interactive props
-              pickable: true,
-              autoHighlight: true,
-              onClick: info =>
-                // eslint-disable-next-line
-                info.object &&
-                alert(
-                  `${info.object.properties.name} (${info.object.properties.abbrev})`
-                ),
-            }),
-            new ArcLayer({
-              id: 'arcs',
-              data: AIR_PORTS,
-              dataTransform: d => d.features.filter(f => f.properties.scalerank < 4),
-              // Styles
-              getSourcePosition: () => [-0.4531566, 51.4709959], // London
-              getTargetPosition: f => f.geometry.coordinates,
-              getSourceColor: [0, 128, 200],
-              getTargetColor: [200, 0, 80],
-              getWidth: 1,
-            }),
+      id: 'base-map',
+      data: USMAP,
+      // Styles
+      stroked: true,
+      filled: true,
+      lineWidthMinPixels: 2,
+      opacity: 0.4,
+      getLineColor: [60, 60, 60],
+      getFillColor: [200, 200, 200],
+    }),
+    new GeoJsonLayer({
+      id: 'geojson-layer',
+      data: DATA_URL,
+      lineWidthScale: 1,
+      lineWidthMinPixels: 2,
+      getLineColor: d => colorToRGBArray(d.properties.speed),
+      getPointRadius: 2,
+      getLineWidth: 3,
+      visible: true,
+  })
   ];
 
   return (<div className="deckgl-container">
