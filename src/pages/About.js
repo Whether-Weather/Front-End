fa15d163ba8eb4e8f533035a740dcfa3748df86b
MalcolmWeaver <69010072+MalcolmWeaver@users.noreply.github.com>
2023-04-21 20:45:31 -0700

    About Us Page More Aesthetic.
    Making About Us Page More Aesthetic. Also, this makes all the page titles look a little clearer

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,35 +1,31 @@
 import background from '../assets/home-background.png';
-import React from "react";
-  
+import snowyCrash from '../assets/SnowyCrash.jpg';
+
 const About = () => {
   return (
     <div>
-        <div className="background-image-container">
-            <img src={background} className="background-image" alt="logo" />
-            <div className="background-text">About Us</div>
-        </div>
-        <div className="grid">
-          <header>
-            <h2 className="about-us-title">
-              <b>Enhancing Transportation Safety in Harsh Weather Conditions</b>
-            </h2>
-          </header>
-          <div className="about-us-content">
-            <p> 
-            On average, nearly 5,000 people are killed and over 418,000 people are injured in weather-related crashes each year. These numbers are likely to increase over time when one considers increases in erratic weather behavior due to climate change.
-            </p>
-            <p>
-            We want to create a website/platform to analyze the relationship between harsh weather and transportation safety. Specifically, our platform will aim to analyze how a variety of weather conditions impact the statistical safety of every segment of road within certain geographical regions. 
-            </p>
-            
+      <div className="background-image-container">
+        <img src={background} className="background-image" alt="logo" />
+        <div className="background-text">About Us</div>
+      </div>
+      <div className="about-us-content-area">
+        <div className="about-us-content">
+          <div className="about-us-title">Enhancing Transportation Safety in Harsh Weather Conditions</div>
+          <div className="about-us-text">
+            <div className="about-us-paragraph"> 
+              On average, nearly 5,000 people are killed and over 418,000 people are injured in weather-related crashes each year. These numbers are likely to increase over time when one considers increases in erratic weather behavior due to climate change.
+            </div>
+            <div className="about-us-paragraph"> 
+              We want to create a website/platform to analyze the relationship between harsh weather and transportation safety. Specifically, our platform will aim to analyze how a variety of weather conditions impact the statistical safety of every segment of road within certain geographical regions.
+            </div>
           </div>
-          
-          
-
-       </div>
+        </div>
+      </div>
+      <div className="about-us-bottom">
+          <img src={snowyCrash} className="about-us-image-container" alt="Snowy Crash" />
+        </div>
     </div>
-
-);
+  );
 };
-  
+
 export default About;
