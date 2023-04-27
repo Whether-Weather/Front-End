3a459ac92c3fe18109ab5d52527481c7182ce8fd
Josh Kelleran <josh.kelleran@gmail.com>
2023-04-26 20:31:37 -0700

    working version!!!!

--- src/pages/LiveMap.js
+++ src/pages/LiveMap.js
@@ -1,47 +1,81 @@
-import React, { useEffect } from 'react';
-import L from 'leaflet';
-import axios from 'axios';
+import React from 'react';
+import DeckGL from '@deck.gl/react';
+import { GeoJsonLayer } from '@deck.gl/layers';
+import { Map } from 'react-map-gl';
+
+
 
 import '../App.css';
 
-const DATA_URL = process.env.PUBLIC_URL + '/data/harriscounty.geojson';
+
+// main key = "sk.eyJ1IjoibWVsbG9qZWxsb2ZlbGxvIiwiYSI6ImNsZ3lpaGppMzA5bXYzaXFxNmZyMGl3ajkifQ.RUd8qDlsz8gsgW6bEUEGyg"
+
+const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoibWVsbG9qZWxsb2ZlbGxvIiwiYSI6ImNsZ3loNDZ3YTA5ZTMzZ3A0bnJtYWtucDQifQ.rwhQ-AcBCdf0q-ouG_5kCA';
+const DATA_URL = process.env.PUBLIC_URL + '/data/output_file.geojson';
+
+
+const MAP_STYLE = 'mapbox://styles/mapbox/streets-v12';
+//'mapbox://styles/mapbox/dark-v10';
+//'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json';
+//"https://tiles.basemaps.cartocdn.com/gl/voyager-gl-style/style.json";
+//'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json'
+//'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json';
+
+
+
+
+const INITIAL_VIEW_STATE = {
+  longitude: -122.7401,
+  latitude: 37.7511,
+  zoom: 7,
+  maxZoom: 16,
+  pitch: 0,
+  bearing: 0
+};
 
 function Livemap() {
-  useEffect(() => {
-    const map = L.map('map').setView([37.7511, -122.7401], 7);
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
-
-    // Fetch and add GeoJSON data to the map
-    axios.get(DATA_URL).then((response) => {
-      const geoJsonData = response.data;
-
-      L.geoJSON(geoJsonData, {
-        style: () => {
-          return {
-            color: '#f00',
-            weight: 3,
-          };
-        },
-      }).addTo(map);
-    });
-
-    return () => {
-      map.remove();
-    };
-  }, []);
+  
+  const layers = [
+    new GeoJsonLayer({
+      id: 'geojson-layer',
+      data: DATA_URL,
+      lineWidthScale: 1,
+      lineWidthMinPixels: 2,
+      getLineColor: [250, 0, 0],
+      getPointRadius: 2,
+      getLineWidth: 3,
+      visible: true,
+    }),
+  ];
 
   return (
     <div>
       <div className="map-search">
         <input type="text" placeholder="Search" className="map-search-text"></input>
       </div>
-      <div id="map" style={{ height: '100%', width: '100%' }} />
+      <div className="map-zoom-container">
+        <div className='map-zoom'>
+          <input type="button" value="+" className="map-zoom-text"></input>
+        </div>
+        <div className='map-zoom'>
+          <input type="button" value="-" className="map-zoom-text"></input>
+        </div>
+      </div>
+      <div className="deckgl-container">
+        <DeckGL
+          initialViewState={INITIAL_VIEW_STATE}
+          controller={true}
+          layers={layers}
+        >
+          <Map 
+            reuseMaps mapStyle={MAP_STYLE} 
+            preventStyleDiffing={false} 
+            mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
+          />
+        </DeckGL>
+      </div>
     </div>
   );
 }
 
 export default Livemap;
-
