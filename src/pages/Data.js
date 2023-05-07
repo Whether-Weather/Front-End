19065369d5bd7890f82fb8c63f7a279ad7fa135e
MalcolmWeaver <69010072+MalcolmWeaver@users.noreply.github.com>
2023-05-06 19:41:02 -0700

    Data Findings Initial Draft

--- src/pages/Data.js
+++ src/pages/Data.js
@@ -1,5 +1,11 @@
 import React from "react";
 import background from "../assets/data-background.png";
+import BusyStreet from "../assets/BusyStreet.jpg";
+import CrashInTheSun from "../assets/CrashInTheSun.jpg";
+import DrivingThroughPuddle from "../assets/DrivingThroughPuddle.jpg";
+import FoggyDrive from "../assets/FoggyDrive.jpg";
+import rushHour from "../assets/rushHour.jpg";
+
 
 const About = () => {
   return (
@@ -8,42 +14,47 @@ const About = () => {
         <img src={background} className="background-image" alt="logo" />
         <div className="background-text">Data Findings</div>
       </div>
-      <div className="body-content-area">
-        <p className="body-block-text">
-          Welcome to the Data Findings section of our website! Here, we present
-          the latest information and insights related to weather and traffic
-          data. Our team of experts regularly analyze and interpret vast amounts
-          of data to provide you with valuable insights that can help you make
-          informed decisions.{" "}
-        </p>
-        <p className="body-block-text">
-          Stay tuned for updates on:
-          <ul>
-            <li>Weather patterns and their impact on traffic flow</li>
-            <li>Traffic congestion trends in different regions</li>
-            <li>Analysis of traffic accidents and their causes</li>
-            <li>
-              The effectiveness of different traffic management strategies
-            </li>
-            <li>Real-time traffic updates and predictions</li>
-          </ul>
-        </p>
-        <p className="body-block-text">
-          We want to create a website/platform to analyze the relationship
-          between harsh weather and transportation safety. Specifically, our
-          platform will aim to analyze how a variety of weather conditions
-          impact the statistical safety of every segment of road within certain
-          geographical regions.
-        </p>
-        <p className="body-block-text">
-          Our goal is to provide you with accurate and up-to-date information
-          that can help you navigate the complex world of traffic and weather
-          conditions. Whether you're a commuter, a transportation professional,
-          or just interested in learning more about this topic, we've got you
-          covered. So, bookmark this page and come back often for the latest
-          data findings!
-        </p>
+
+      <div className="data-findings-section-container">
+        <img src={DrivingThroughPuddle} className="data-findings-image-container" alt="Driving Through A Puddle" /> 
+        <div className="data-findings-title-over-text">
+          <div className="data-findings-title">Seattle Drivers</div>
+          <div className="data-findings-text">Seattle drivers are well-equipped to handle bad weather, as evidenced by the minimal impact of weather conditions on traffic speed.</div>
+        </div>
       </div>
+
+      <div className="data-findings-section-container">
+        <div className="data-findings-title-over-text">
+          <div className="data-findings-title">Santa Clara Drivers</div>
+          <div className="data-findings-text">Weather conditions can significantly impact traffic speed in different cities, such as Santa Clara, where drivers may be less experienced in driving in bad weather.</div>
+        </div>
+        <img src={CrashInTheSun} className="data-findings-image-container" alt="Crash In The Sun" /> 
+      </div>
+      
+      <div className="data-findings-section-container">
+        <img src={BusyStreet} className="data-findings-image-container" alt="Rush Hour" /> 
+        <div className="data-findings-title-over-text">
+          <div className="data-findings-title">What Increases Traffic?</div>
+          <div className="data-findings-text">Traffic speed slowdowns are influenced by a complex interplay of factors, including weather conditions and driving culture.</div>
+        </div>
+      </div>
+
+      <div className="data-findings-section-container">
+        <div className="data-findings-title-over-text">
+          <div className="data-findings-title">Time Of Day</div>
+          <div className="data-findings-text">Time of day has one of the strongest correlations with traffic speed, with the highest slowdowns occurring during rush hour.</div>
+        </div>
+        <img src={rushHour} className="data-findings-image-container" alt="Crash In The Sun" /> 
+      </div>
+      
+      <div className="data-findings-section-container">
+        <img src={FoggyDrive} className="data-findings-image-container" alt="Driving Through A Puddle" /> 
+        <div className="data-findings-title-over-text">
+          <div className="data-findings-title">Humidity and Percipitation</div>
+          <div className="data-findings-text">Relative humidity has a higher correlation with traffic speed than precipitation, indicating that high humidity can lead to reduced visibility and more cautious driving.</div>
+        </div>
+      </div>
+
     </div>
   );
 };
