8a6cf57633c03053daaf0073e7e27b86c3171ef9
JoshKelleran <josh.kelleran@gmail.com>
2023-05-23 20:59:18 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,12 +10,9 @@ ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
+  <App />
 );
+
 
 reportWebVitals();
 
